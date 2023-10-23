3e0c135bae1f34f9cd092f1318bff1d9632f779a
enurugal <131149920+enurugal@users.noreply.github.com>
2023-10-24 00:37:52 +0300

    Create Migratory_Birds.go

---
+++ Migratory_Birds.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func migratoryBirds(arr []int32) int32 {
+    // Create a map to count the frequency of each bird type.
+    birdCount := make(map[int32]int32)
+    maxCount := int32(0)
+    minID := int32(0)
+
+    for _, bird := range arr {
+        birdCount[bird]++
+        if birdCount[bird] > maxCount || (birdCount[bird] == maxCount && bird < minID) {
+            maxCount = birdCount[bird]
+            minID = bird
+        }
+    }
+
+    return minID
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    arrCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+
+    arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var arr []int32
+
+    for i := 0; i < int(arrCount); i++ {
+        arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+        checkError(err)
+        arrItem := int32(arrItemTemp)
+        arr = append(arr, arrItem)
+    }
+
+    result := migratoryBirds(arr)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
